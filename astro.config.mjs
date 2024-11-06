1b4a1071f89339b7a8197083cf2e07c64a3df41e
kalmai <leozverres@gmail.com>
2024-11-05 22:06:04 -0500

    added github workflow and gitpage config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kalmai.github.io',
+  base: 'da-portfolio',
+});
